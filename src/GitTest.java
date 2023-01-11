2c8785022118ccf5be7ff44587aefefb901b6c6b
sunshine556 <122357028+sunshine556@users.noreply.github.com>
2023-01-11 21:32:23 +0800

    pull commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master java!");
         System.out.println("hot-fix java!");
         System.out.println("pushtest java!");
+        System.out.println("pulltest java!");
 
 
     }
