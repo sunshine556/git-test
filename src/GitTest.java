2439ba6c6f1339a3064da682921b2fc0c2f31a30
sunshine556 <122357028+sunshine556@users.noreply.github.com>
2023-01-11 22:29:57 +0800

    github update

--- src/GitTest.java
+++ src/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hot-fix java!");
         System.out.println("pushtest java!");
         System.out.println("pulltest java!");
+        github更新
 
 
     }
